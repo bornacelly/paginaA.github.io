5ed01d5e1870b1a1caaa1ebc63067a7b94e46494
bornacelly <anotherherodapa@gmail.com>
2026-07-27 15:24:05 -0500

    Add initial HTML, CSS, and JavaScript files for user registration feature

---
+++ script.js
@@ -0,0 +1,40 @@
+let usuarioActual = null;
+
+function registrar(){
+
+const usuario = document.getElementById("usuario").value;
+
+const password = document.getElementById("password").value;
+
+if(usuario==="" || password===""){
+
+alert("Complete todos los campos");
+
+return;
+
+}
+
+usuarioActual={
+
+usuario:usuario,
+password:password
+
+};
+
+document.getElementById("registro").style.display="none";
+
+document.getElementById("menu").style.display="block";
+
+document.getElementById("bienvenida").innerHTML=
+"Bienvenido "+usuario;
+
+}
+
+function irPaginaB(){
+
+const token=btoa(JSON.stringify(usuarioActual));
+
+window.location.href=
+"https://bornacelly.github.io/PaginaB/?token="+token;
+
+}
